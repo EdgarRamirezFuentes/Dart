4189f21e17cdffd6af28129d32c42f63c9df0076
Edgar Ramirez <edgar-alejandro-fuentes@hotmail.com>
2019-12-09 16:39:35 -0600

    Todos los tipos de datos incluidos.

--- Ejercicios/Basicos/02_Tipos_de_datos.dart
+++ Ejercicios/Basicos/02_Tipos_de_datos.dart
@@ -1,12 +1,12 @@
 main()
 {
-  // Números
+  //================== Números
 
   int a = 10;
   double b = 10.55;
   int c = null;
 
-  // Strings
+  //================== Strings
 
   String nombre = 'Edgar';
   String nombre2 = "Alejandro";
@@ -18,11 +18,41 @@ main()
 """Hola mundo
 ¿Como están?""";
 
-  // Boolean
+  //================== Boolean
 
-  // Listas - Arreglos
+  bool booleano = true;
+  bool booleano2 = false;
+  bool booleano3 = null;
+    
 
-  // Sets 
+  //================== Listas - Arreglos
 
-  // Mapas
+  List <String> heroes = new List();
+  heroes.add('The flash');
+  heroes.add('Batman');
+  heroes..add('Green arrow')
+        ..add('Wonder woman'); // Operador cascada.
+  heroes.addAll(['Aquaman','Green lantern', 'Shazam']);
+  List <String> villanos = new List(3);
+  // No es permitido utilizar .add o .addAll en una lista estática.
+  villanos[0] = 'Reverse flash';
+  villanos[1] = 'Gorila Grod';
+  villanos[2] = 'Killer Frost';
+  // Esto desbordaría la memoría --> villanos[3] = ' Capitán boomerang';
+
+
+  //================== Sets 
+
+  Set <String> villanos2 = {'Lex luthor', 'Bizarro', 'Lobo'};
+  villanos2.add('Lobo'); // No lo va a agregar de nuevo.
+
+  //================== Mapas
+
+  Map <String, String> ironman = {
+    'nombre': 'Tony Stark',
+    'poderes': 'Inteligencia y dinero'
+  };
+
+  Map <String, dynamic> capitan = new Map();
+  capitan.addAll({'nombre': 'Steve Rogers', 'edad': 10});
 }

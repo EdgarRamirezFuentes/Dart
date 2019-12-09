ca2614cedf5e33799912a4451b9eaac83572fc52
Edgar Ramirez <edgar-alejandro-fuentes@hotmail.com>
2019-12-09 00:30:15 -0600

    Declaracion de variables de tipo Strings.

---
+++ Ejercicios/Basicos/02_Tipos_de_datos.dart
@@ -0,0 +1,28 @@
+main()
+{
+  // Números
+
+  int a = 10;
+  double b = 10.55;
+  int c = null;
+
+  // Strings
+
+  String nombre = 'Edgar';
+  String nombre2 = "Alejandro";
+  String nombre3 = 'O\' Connor';
+  String multilinea =
+'''Hola mundo
+¿Como están?''';
+  String multilinea2 = 
+"""Hola mundo
+¿Como están?""";
+
+  // Boolean
+
+  // Listas - Arreglos
+
+  // Sets 
+
+  // Mapas
+}
